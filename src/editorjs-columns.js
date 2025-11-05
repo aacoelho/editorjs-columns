b6d4d1e94d480a591890c6be67f23e918d4a67b8
Ângela Coelho <angela@pr.co>
2025-11-05 12:30:21 +0000

    Add possibility to have 4 columns

--- src/editorjs-columns.js
+++ src/editorjs-columns.js
@@ -91,18 +91,23 @@ class EditorJsColumns {
 	renderSettings() {
 		return [
 			{
-				icon : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 641 640"><path d="M65 174h232.727v292H65V174Zm279.273 0H577v292H344.273V174Z" fill="#000000" fill-rule="nonzero"/></svg>`,
+				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM152,168a8,8,0,0,1,0,16H104a8,8,0,0,1-6.4-12.8l43.17-57.56a16,16,0,1,0-27.86-15,8,8,0,0,1-15.09-5.34,32,32,0,1,1,55.74,29.93L120,168Z"></path></svg>`,
 				label : this.api.i18n.t("2 Columns"),
 				onActivate : () => {this._updateCols(2)}
 			},
 			{
-				icon : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 641 640"><path d="M30 175h162.96v292H30V175Zm209.52 0h162.96v292H239.52V175Zm209.52 0H612v292H449.04V175Z" fill="#000000" fill-rule="nonzero"/></svg>`,
+				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM124,184a35.71,35.71,0,0,1-25.71-10.81A8,8,0,1,1,109.71,162,20,20,0,1,0,124,128a8,8,0,0,1-6.55-12.59L136.63,88H104a8,8,0,0,1,0-16h48a8,8,0,0,1,6.55,12.59l-21,30A36,36,0,0,1,124,184Z"></path></svg>`,
 				label : this.api.i18n.t("3 Columns"),
 				onActivate : () => {this._updateCols(3)}
 			},
 			{
-				icon : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M319.5 87.273V0L203.25 116.364 319.5 232.727v-87.272c96.342 0 174.375 78.109 174.375 174.545 0 29.527-7.41 57.164-20.198 81.6l42.43 42.473C538.632 408 552 365.673 552 320c0-128.582-104.044-232.727-232.5-232.727Zm0 407.272c-96.342 0-174.375-78.109-174.375-174.545 0-29.527 7.41-57.164 20.198-81.6l-42.43-42.473C100.368 232 87 274.327 87 320c0 128.582 104.044 232.727 232.5 232.727V640l116.25-116.364L319.5 407.273v87.272Z" fill="#000000" fill-rule="nonzero"/></svg>`,
-				label : this.api.i18n.t("Rearrange Columns"),
+				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM160,160h-8v16a8,8,0,0,1-16,0V160H88a8,8,0,0,1-6.31-12.91l56-72A8,8,0,0,1,152,80v64h8a8,8,0,0,1,0,16Zm-55.64-16L136,103.32V144Z"></path></svg>`,
+				label : this.api.i18n.t("4 Columns"),
+				onActivate : () => {this._updateCols(4)}
+			},
+			{
+				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M88,104H40a8,8,0,0,1-8-8V48a8,8,0,0,1,13.66-5.66L64,60.7a95.42,95.42,0,0,1,66-26.76h.53a95.36,95.36,0,0,1,67.07,27.33,8,8,0,0,1-11.18,11.44,79.52,79.52,0,0,0-55.89-22.77h-.45A79.48,79.48,0,0,0,75.35,72L93.66,90.34A8,8,0,0,1,88,104Zm128,48H168a8,8,0,0,0-5.66,13.66L180.65,184a79.48,79.48,0,0,1-54.72,22.09h-.45a79.52,79.52,0,0,1-55.89-22.77,8,8,0,1,0-11.18,11.44,95.36,95.36,0,0,0,67.07,27.33H126a95.42,95.42,0,0,0,66-26.76l18.36,18.36A8,8,0,0,0,224,208V160A8,8,0,0,0,216,152Z"></path></svg>`,
+				label : this.api.i18n.t("Roll Columns"),
 				onActivate : () => {this._rollColumns()}
 			},
 			]
@@ -117,30 +122,33 @@ class EditorJsColumns {
 	}
 
 	async _updateCols(num) {
-		// Should probably update to make number dynamic... but this will do for now
-		if (num == 2) {
-			if (this.editors.numberOfColumns == 3) {
-				let resp = await Swal.fire({
-					title: this.api.i18n.t("Are you sure?"),
-					text: this.api.i18n.t("This will delete Column 3!"),
-					icon: "warning",
-					showCancelButton: true,
-					cancelButtonText: this.api.i18n.t("Cancel"),
-					confirmButtonColor: "#3085d6",
-					cancelButtonColor: "#d33",
-					confirmButtonText: this.api.i18n.t("Yes, delete it!"),
-				});
+		const newNumberOfColumns = num;
+		const currentNumberOfColumns = this.editors.numberOfColumns;
+		
+		if (newNumberOfColumns < currentNumberOfColumns) {
+			const columnsToDelete = currentNumberOfColumns - newNumberOfColumns;
+			let resp = await Swal.fire({
+				title: this.api.i18n.t("Are you sure?"),
+				text: this.api.i18n.t(`This will delete the last ${columnsToDelete ==="1" ? "column" : `${columnsToDelete} columns`}!`),
+				icon: "warning",
+				showCancelButton: true,
+				cancelButtonText: this.api.i18n.t("Cancel"),
+				confirmButtonColor: "#3085d6",
+				cancelButtonColor: "#d33",
+				confirmButtonText: this.api.i18n.t("Yes, delete it!"),
+			});
 
-				if (resp.isConfirmed) {
-					this.editors.numberOfColumns = 2;
+			if (resp.isConfirmed) {
+				this.editors.numberOfColumns = newNumberOfColumns;
+				for (let index = 0; index < columnsToDelete; index++) {
 					this.data.cols.pop();
 					this.editors.cols.pop();
-					this._rerender();
 				}
+				this._rerender();
 			}
 		}
-		if (num == 3) {
-			this.editors.numberOfColumns = 3;
+		else if (newNumberOfColumns > currentNumberOfColumns) {
+			this.editors.numberOfColumns = newNumberOfColumns;
 			this._rerender();
 		}
 	}
