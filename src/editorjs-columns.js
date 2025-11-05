e8aa541f9b6962bbeca8d2a46462eb4c1b99ba09
Ângela Coelho <angela@pr.co>
2025-11-05 14:56:19 +0000

    Update logic of adding and removing columns

--- src/editorjs-columns.js
+++ src/editorjs-columns.js
@@ -91,26 +91,27 @@ class EditorJsColumns {
 	renderSettings() {
 		return [
 			{
-				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM152,168a8,8,0,0,1,0,16H104a8,8,0,0,1-6.4-12.8l43.17-57.56a16,16,0,1,0-27.86-15,8,8,0,0,1-15.09-5.34,32,32,0,1,1,55.74,29.93L120,168Z"></path></svg>`,
-				label : this.api.i18n.t("2 Columns"),
-				onActivate : () => {this._updateCols(2)}
+				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M96,48V208a16,16,0,0,1-16,16H56a16,16,0,0,1-16-16V48A16,16,0,0,1,56,32H80A16,16,0,0,1,96,48Zm56-16H128a16,16,0,0,0-16,16V208a16,16,0,0,0,16,16h24a16,16,0,0,0,16-16V48A16,16,0,0,0,152,32Zm88,88H224V104a8,8,0,0,0-16,0v16H192a8,8,0,0,0,0,16h16v16a8,8,0,0,0,16,0V136h16a8,8,0,0,0,0-16Z"></path></svg>`,
+				name: `add-column`,
+				label : this.api.i18n.t("Add column"),
+				isDisabled: this.editors.numberOfColumns === 5,
+				onActivate : () => {this._updateCols(1)}
 			},
 			{
-				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM124,184a35.71,35.71,0,0,1-25.71-10.81A8,8,0,1,1,109.71,162,20,20,0,1,0,124,128a8,8,0,0,1-6.55-12.59L136.63,88H104a8,8,0,0,1,0-16h48a8,8,0,0,1,6.55,12.59l-21,30A36,36,0,0,1,124,184Z"></path></svg>`,
-				label : this.api.i18n.t("3 Columns"),
-				onActivate : () => {this._updateCols(3)}
-			},
-			{
-				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM160,160h-8v16a8,8,0,0,1-16,0V160H88a8,8,0,0,1-6.31-12.91l56-72A8,8,0,0,1,152,80v64h8a8,8,0,0,1,0,16Zm-55.64-16L136,103.32V144Z"></path></svg>`,
-				label : this.api.i18n.t("4 Columns"),
-				onActivate : () => {this._updateCols(4)}
+				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M224,56a8,8,0,0,1-8,8h-8V208a16,16,0,0,1-16,16H64a16,16,0,0,1-16-16V64H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,56ZM88,32h80a8,8,0,0,0,0-16H88a8,8,0,0,0,0,16Z"></path></svg>`,
+				name: `remove-column`,
+				label : this.api.i18n.t("Remove column"),
+				isDisabled: this.editors.numberOfColumns === 1,
+				onActivate : () => {this._updateCols(-1)}
 			},
 			{
 				icon : `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M88,104H40a8,8,0,0,1-8-8V48a8,8,0,0,1,13.66-5.66L64,60.7a95.42,95.42,0,0,1,66-26.76h.53a95.36,95.36,0,0,1,67.07,27.33,8,8,0,0,1-11.18,11.44,79.52,79.52,0,0,0-55.89-22.77h-.45A79.48,79.48,0,0,0,75.35,72L93.66,90.34A8,8,0,0,1,88,104Zm128,48H168a8,8,0,0,0-5.66,13.66L180.65,184a79.48,79.48,0,0,1-54.72,22.09h-.45a79.52,79.52,0,0,1-55.89-22.77,8,8,0,1,0-11.18,11.44,95.36,95.36,0,0,0,67.07,27.33H126a95.42,95.42,0,0,0,66-26.76l18.36,18.36A8,8,0,0,0,224,208V160A8,8,0,0,0,216,152Z"></path></svg>`,
-				label : this.api.i18n.t("Roll Columns"),
+				name: `roll-columns`,
+				label : this.api.i18n.t("Roll columns"),
+				isDisabled: this.editors.numberOfColumns === 1,
 				onActivate : () => {this._rollColumns()}
 			},
-			]
+		]
 	}
 
 
@@ -122,14 +123,25 @@ class EditorJsColumns {
 	}
 
 	async _updateCols(num) {
-		const newNumberOfColumns = num;
-		const currentNumberOfColumns = this.editors.numberOfColumns;
+		const newNumberOfColumns = this.editors.numberOfColumns + num;
 		
-		if (newNumberOfColumns < currentNumberOfColumns) {
-			const columnsToDelete = currentNumberOfColumns - newNumberOfColumns;
+		// Add column
+		if (num === 1) {
+			if(newNumberOfColumns > 5) {
+				return;
+			}
+			this.editors.numberOfColumns = newNumberOfColumns;
+			this._rerender();
+		}
+		// Remove column
+		if (num === -1) {
+			if(newNumberOfColumns < 1) {
+				return;
+			}
+
 			let resp = await Swal.fire({
 				title: this.api.i18n.t("Are you sure?"),
-				text: this.api.i18n.t(`This will delete the last ${columnsToDelete ==="1" ? "column" : `${columnsToDelete} columns`}!`),
+				text: this.api.i18n.t(`This will delete the last column!`),
 				icon: "warning",
 				showCancelButton: true,
 				cancelButtonText: this.api.i18n.t("Cancel"),
@@ -140,16 +152,10 @@ class EditorJsColumns {
 
 			if (resp.isConfirmed) {
 				this.editors.numberOfColumns = newNumberOfColumns;
-				for (let index = 0; index < columnsToDelete; index++) {
-					this.data.cols.pop();
-					this.editors.cols.pop();
-				}
+				this.data.cols.pop();
+				this.editors.cols.pop();
 				this._rerender();
 			}
-		}
-		else if (newNumberOfColumns > currentNumberOfColumns) {
-			this.editors.numberOfColumns = newNumberOfColumns;
-			this._rerender();
 		}
 	}
 
